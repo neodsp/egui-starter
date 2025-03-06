94abb90c37ce4a5b73b054373d37337f26330bbe
Stephan Eckes <stephan@neodsp.com>
2025-03-06 21:53:19 +0100

    Plot (#1)
    * plot starter
    
    * change name

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = "egui-template-pwa";
 var filesToCache = [
   "./",
   "./index.html",
-  "./egui-demo-2c4f329c7cb8aea3.js",
-  "./egui-demo-2c4f329c7cb8aea3_bg.wasm",
+  "./egui-starter-2c4f329c7cb8aea3.js",
+  "./egui-starter-2c4f329c7cb8aea3_bg.wasm",
 ];
 
 /* Start the service worker and cache all of the app's content */
