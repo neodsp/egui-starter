924e06685815d70978a4f2e4363a1bae9a255186
steckes <stephan@neodsp.com>
2025-03-06 21:50:45 +0100

    plot starter

--- src/app.rs
+++ src/app.rs
@@ -15,6 +15,18 @@ impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Hello World!");
+            use egui_plot::{Line, Plot, PlotPoints};
+
+            let sin: PlotPoints = (0..1000)
+                .map(|i| {
+                    let x = i as f64 * 0.01;
+                    [x, x.sin()]
+                })
+                .collect();
+            let line = Line::new(sin);
+            Plot::new("my_plot")
+                .view_aspect(2.0)
+                .show(ui, |plot_ui| plot_ui.line(line));
         });
     }
 }
